377942f84ae22ba64de13a8ba79ac29c919215d0
gabay147 <felixgabay@outlook.com>
2026-06-17 16:22:12 -0500

    Tested pulling mapping from API with requests - success.

--- wiki-scraper.py
+++ wiki-scraper.py
@@ -6,12 +6,23 @@ https://oldschool.runescape.wiki/w/RuneScape:Real-time_Prices
 Code version 0.0.1 by github.com/gabay147
 """
 
+# Install packages
+import requests
+
 # Set credentials
 headers = {
     'User-Agent':'GE Data Collector by @_deja.vu on Discord',
 }
 
-base_target = "prices.runescape.wiki/api/v1/osrs"
+base_target = "https://prices.runescape.wiki/api/v1/osrs"
+
+# Try getting base_target + /mapping
+req = requests.get(base_target + '/mapping', headers=headers)
+
+print(req.status_code)
+print(req.text)
+data = req.json()
+print(data)
 # response = requests.get(url, headers=headers)
 
 # BEGIN SCRAPING CODE
